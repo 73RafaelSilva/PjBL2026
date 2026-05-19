2770b59f5332a68d469045fa760b1f5608a544d9
ArthMC <arthurm.colodel@gmail.com>
2026-05-18 22:50:02 -0300

    Grupo.java

---
+++ class/Grupo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.ArrayListist;
+
+public class Grupo {
+
+    private ArrayList<Pessoas> pessoas;
+    
+    private ArrayList<PessoasAdm> pessoasAdm;
+
+    private ArrayList<String> interesses;
+
+    private ArrayList<Evento> evento;
+
+    private String nome;
+
+}
