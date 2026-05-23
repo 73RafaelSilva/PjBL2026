ce6af3d03cd08b79392924c4895a56a80a3ae5ae
73RafaelSilva <73rafael.luiz@gmail.com>
2026-05-22 22:20:59 -0300

    subindo prototipo_telas

--- main/testes.java
+++ main/testes.java
@@ -2,13 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main{
+
+    boolean sessao = false;
+
     public static void main(String args[]){
-        JFrame janela = new JFrame(title: '22/05');
+
+
+        JFrame janela = new JFrame('Gerenciador de Grupos 2.000');
         
-        janela.setSize(width:800, heigth:400);
+        janela.setSize(800, 400);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
-        JLabel label = new JLabel('OLA');
+        JLabel label = new JLabel('Seja bem vindo ao Gerenciador de grupos');
         JPanel painel = new JPanel(null);
 
         painel.add(label);
@@ -17,7 +22,23 @@ public class Main{
         label.setBounds(300/2, 150/2, dimencao.width, dimencao.heigth);
 
         janela.add(painel);
-        janela.setSize(300,150);
+        janela.setSize(300,150); 
         janela.setVisible(b: true);
+
+
+        // Loop da Tela de Login
+        if (this.sessao){
+
+        } else{
+            this.login();
+        }
+
     }
+
+    public void login(){
+        
+
+
+    }
+
 }
