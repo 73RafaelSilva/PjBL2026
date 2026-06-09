cf7ba444d66498bf5736fd73e54cb8ee3eed6161
ArthMC <arthurm.colodel@gmail.com>
2026-06-08 22:41:13 -0300

    Create Paineis.java

---
+++ Paineis.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Paineis {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("enentos");
+        janela.setSize(1024, 765);
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+/*
+ setsize tamanho 
+ setvisible se é visivel ou não
+ setDefaltCloseOperation
+
+
+
+
+
+
+
+
+
+*/
