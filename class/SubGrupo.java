66013945e30c84137600ae20d567b276ae6b5b90
Henriqu07 <125375240+Henriqu07@users.noreply.github.com>
2026-06-12 19:51:39 -0300

    Implement Serializable in SubGrupo class
    Added Serializable interface to SubGrupo class.

--- class/SubGrupo.java
+++ class/SubGrupo.java
@@ -1,4 +1,10 @@
-public class SubGrupo extends Grupo {
+package classes;
+
+import java.io.Serializable;
+
+public class SubGrupo extends Grupo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String descricao;
 
