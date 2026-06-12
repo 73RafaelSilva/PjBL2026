a6f6b00f25ed782c11e769e527f3da505ff0191c
Henriqu07 <125375240+Henriqu07@users.noreply.github.com>
2026-06-12 19:49:39 -0300

    Implement serialization for Agenda class
    Added serialization support and methods to save and load the agenda.

--- class/Agenda.java
+++ class/Agenda.java
@@ -1,14 +1,22 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
-public class Agenda {
+public class Agenda implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private ArrayList<Evento> eventos;
 
+    public Agenda() {
+        eventos = new ArrayList<>();
+    }
+
     public void adicionarEvento(Evento evento) {
         eventos.add(evento);
-
-
     }
 
     public void listarEventos() {
@@ -20,8 +28,45 @@ public class Agenda {
         System.out.println("Agenda de eventos");
 
         for (Evento evento : eventos) {
-            System.out.println(evento);
+            System.out.println(evento.mostrarEvento());
             System.out.println("");
+        }
+    }
+
+    public void salvarAgenda() {
+        try {
+            ObjectOutputStream out =
+                    new ObjectOutputStream(
+                            new FileOutputStream("agenda.dat"));
+
+            out.writeObject(this);
+            out.close();
+
+            System.out.println("Agenda salva com sucesso!");
+
+        } catch (Exception e) {
+            System.out.println("Erro ao salvar agenda.");
+            e.printStackTrace();
+        }
+    }
+
+    public static Agenda carregarAgenda() {
+        try {
+            ObjectInputStream in =
+                    new ObjectInputStream(
+                            new FileInputStream("agenda.dat"));
+
+            Agenda agenda = (Agenda) in.readObject();
+
+            in.close();
+
+            System.out.println("Agenda carregada com sucesso!");
+
+            return agenda;
+
+        } catch (Exception e) {
+            System.out.println("Arquivo não encontrado. Criando nova agenda.");
+            return new Agenda();
         }
     }
 }
